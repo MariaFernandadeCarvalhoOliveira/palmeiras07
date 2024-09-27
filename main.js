efff180ec3c9e6751f9613e9476bbea0839d2493
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-09-27 11:55:17 -0300

    d

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function soma (num1,num2){
 return num1+num2;   
 }
 console.log(soma(6,4));
+function areaQuadrado(lado){
+return lado**2;   
+}
+console.log(areaQuadrado(6));
+let media = function(){
+    return
+}
