237e0ed4052cae304973dda66fb50577293986c9
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-10-04 11:24:07 -0300

    tr

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-let nomeUsuário = 'ketelly'
-alert('ketelly')
-function soma (num1,num2){
-return num1+num2;   
-}
-console.log(soma(6,4));
-function areaQuadrado(lado){
-return lado**2;   
-}
-console.log(areaQuadrado(6));
-let media = function(x, y, z ){
-    return (x+y+z)/3;
-}
-console.log(media(1,2,3));
-parseInt(prompt())
+// let nomeUsuário = 'ketelly'
+// alert('ketelly')
+// function soma (num1,num2){
+// return num1+num2;   
+// }
+// console.log(soma(6,4));
+// function areaQuadrado(lado){
+// return lado**2;   
+// }
+// console.log(areaQuadrado(6));
+// let media = function(x, y, z ){
+//     return (x+y+z)/3;
+// }
+// console.log(media(1,2,3));
+// parseInt(prompt('Diga três valores para eu tirar a média?'));
+// let num1 = parseInt(prompt('Qual nº'));
+// let num2 = parseInt(prompt('Qual nº'));
+// let num3 = parseInt(prompt('Qual nº'));
+// function media (x,y,z){
+//     return(x+y+z) /3;
+// }
+// document.write(media(num1,num2,num3));
