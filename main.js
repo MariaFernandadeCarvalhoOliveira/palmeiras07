82831aa207907a4e3302ee34e9c474d048cce181
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-10-04 12:07:16 -0300

    gherath

--- main.js
+++ main.js
@@ -20,11 +20,17 @@
 // // }
 // // console.log(media(1,2,3));
 // document.write(mediaTresvalores);
-let estados= 
-['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
-let i=0
-while (i<27){
-    console.log(i);
-    console.log(estados[i]);
-i++;
+// let estados= 
+// ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
+// let i=0
+// while (i<27){
+//     console.log(i);
+//     console.log(estados[i]);
+// i++;
+// }
+for (let index = 0; index < estados.length; index++) {
+    const element = array[index];
+    console.log
 }
+
+
