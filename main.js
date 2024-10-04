a387e8c7a5e7647f656f20f3f43c5719e12165ed
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-10-04 11:47:13 -0300

    dd

--- main.js
+++ main.js
@@ -22,3 +22,4 @@
 document.write(mediaTresvalores);
 let estados= 
 ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
+let
