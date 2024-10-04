b6511d0de809fd84a126baa2717523127aaf2a0b
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-10-04 11:39:39 -0300

    y36

--- main.js
+++ main.js
@@ -8,15 +8,18 @@
 // return lado**2;   
 // }
 // console.log(areaQuadrado(6));
-// let media = function(x, y, z ){
-//     return (x+y+z)/3;
+// let media = function(num1,num2,num3)/3
+//     return (num1+num2+num3)/3
+// }
+// let mediaTresvalores = ('Média de três valores')
+// let num1 = parseInt(prompt('Qual o número?'));
+// let num2 = parseInt(prompt('Qual o número?'));
+// let num3 = parseInt(prompt('Qual o número?'));
+// function media (x,y,z){
+//     return(x+y+z)/z;
 // }
 // console.log(media(1,2,3));
-// parseInt(prompt('Diga três valores para eu tirar a média?'));
-// let num1 = parseInt(prompt('Qual nº'));
-// let num2 = parseInt(prompt('Qual nº'));
-// let num3 = parseInt(prompt('Qual nº'));
-// function media (x,y,z){
-//     return(x+y+z) /3;
-// }
-// document.write(media(num1,num2,num3));
+document.write(mediaTresvalores);
+let estados= 
+["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"];
+estados.length
