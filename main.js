326967215e62db688942b748877a7a3443137dbe
maria_fernanda <142353157+MariaFernandadeCarvalhoOliveira@users.noreply.github.com>
2024-10-04 11:47:54 -0300

    .

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-// let nomeUsuário = 'ketelly'
-// alert('ketelly')
-// function soma (num1,num2){
-// return num1+num2;   
-// }
-// console.log(soma(6,4));
-// function areaQuadrado(lado){
-// return lado**2;   
-// }
-// console.log(areaQuadrado(6));
-// let media = function(num1,num2,num3)/3
-//     return (num1+num2+num3)/3
-// }
-// let mediaTresvalores = ('Média de três valores')
-// let num1 = parseInt(prompt('Qual o número?'));
-// let num2 = parseInt(prompt('Qual o número?'));
-// let num3 = parseInt(prompt('Qual o número?'));
-// function media (x,y,z){
-//     return(x+y+z)/z;
-// }
-// console.log(media(1,2,3));
-document.write(mediaTresvalores);
+// // let nomeUsuário = 'ketelly'
+// // alert('ketelly')
+// // function soma (num1,num2){
+// // return num1+num2;   
+// // }
+// // console.log(soma(6,4));
+// // function areaQuadrado(lado){
+// // return lado**2;   
+// // }
+// // console.log(areaQuadrado(6));
+// // let media = function(num1,num2,num3)/3
+// //     return (num1+num2+num3)/3
+// // }
+// // let mediaTresvalores = ('Média de três valores')
+// // let num1 = parseInt(prompt('Qual o número?'));
+// // let num2 = parseInt(prompt('Qual o número?'));
+// // let num3 = parseInt(prompt('Qual o número?'));
+// // function media (x,y,z){
+// //     return(x+y+z)/z;
+// // }
+// // console.log(media(1,2,3));
+// document.write(mediaTresvalores);
 let estados= 
 ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
 let
